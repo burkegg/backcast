9f25d148d161bb0de7158c2b3ca47f54eedfaae4
burkegg <burkegreen@gmail.com>
2018-06-25 21:51:31 -0700

    Working on backcast.  Just got views to load.

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,9 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
-
+  model: Video,
+  // data: window.exampleVideoData,
+  
+  // var printout = function() {
+  //   console.log(this.)
+  // }
 });
--- src/views/app.js
+++ src/views/app.js
@@ -1,9 +1,13 @@
 var AppView = Backbone.View.extend({
 
   el: '#app',
+  
 
   initialize: function() {
     this.videos = new Videos();
+    this.render();
+    this.renderVideoPlayView();
+    this.renderVideoListView();
   },
 
 
@@ -11,6 +15,22 @@ var AppView = Backbone.View.extend({
     this.$el.html(this.template());
     return this;
   },
+
+  renderVideoPlayView: function() {
+    var videoPlayview = new VideoPlayerView({
+      el: this.$('.player'),
+      collection: this.collection,
+    }).render();
+  },
+  
+  renderVideoListView: function() {
+   new VideoListView({
+     el: this.$('.list'),
+     collection: this.collection,
+   }).render();
+
+  },
+
 
   template: templateURL('src/templates/app.html')
 
